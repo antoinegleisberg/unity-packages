3952366725b01fb1249773ca4e5d027059e0c7b0
Antoine Gleisberg <antoine.gleisberg@gmail.com>
2024-05-01 17:48:23 +0200

    adding methods to calculate difference between dictionaries content and for closest point among vectors

--- Types/Runtime/MethodExtensions.cs
+++ Types/Runtime/MethodExtensions.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -35,6 +34,49 @@ namespace antoinegleisberg.Types
             }
 
             return dict;
+        }
+
+        /// <summary>
+        /// Returns a dictionary containing the difference between the two dictionaries.
+        /// The returned dictionary contains the keys that are in dict1, witht the value being the difference between the value in dict1 and the value in dict2.
+        /// All values in both dict1 and dict2 are assumed to be positive or 0
+        /// In other words, dict1.Diff(dict2)[key] = dict1[key] - dict2.get(key, 0)
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="dict1"></param>
+        /// <param name="dict2"></param>
+        /// <returns></returns>
+        public static Dictionary<TKey, int> Diff<TKey>(this IReadOnlyDictionary<TKey, int> dict1, IReadOnlyDictionary<TKey, int> dict2)
+        {
+            Dictionary<TKey, int> result = new Dictionary<TKey, int>();
+            foreach (TKey key in dict1.Keys)
+            {
+                int value = dict1[key] - dict2.GetValueOrDefault(key, 0);
+                if (value > 0)
+                {
+                    result[key] = value;
+                }
+            }
+            return result;
+        }
+
+        public static Vector3 ClosestTo(this List<Vector3> vectors, Vector3 target)
+        {
+            Vector3 closest = vectors[0];
+            float closestDistance = Vector3.Distance(vectors[0], target);
+
+            for (int i = 1; i < vectors.Count; i++)
+            {
+                float distance = Vector3.Distance(vectors[i], target);
+
+                if (distance < closestDistance)
+                {
+                    closest = vectors[i];
+                    closestDistance = distance;
+                }
+            }
+
+            return closest;
         }
     }
 }
