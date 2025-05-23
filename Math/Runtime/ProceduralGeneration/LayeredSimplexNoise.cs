b521230cc0d7c841d2707e58ab0ffabfc569bfb6
Antoine Gleisberg <antoine.gleisberg@gmail.com>
2025-05-23 16:08:32 +0200

    updating simplex noise with layered noise

--- Math/Runtime/ProceduralGeneration/LayeredSimplexNoise.cs
+++ Math/Runtime/ProceduralGeneration/LayeredSimplexNoise.cs
@@ -7,15 +7,26 @@ namespace antoinegleisberg.Math.ProceduralGeneration
 {
     public class LayeredSimplexNoise
     {
-        private BaseSimplexNoise _baseNoise;
+        private List<BaseSimplexNoise> _baseNoises;
+        private int _seed;
 
         public LayeredSimplexNoise(int seed)
         {
-            _baseNoise = new BaseSimplexNoise(seed);
+            _baseNoises = new List<BaseSimplexNoise>();
+            _baseNoises.Add(new BaseSimplexNoise(seed));
+            _seed = seed;
         }
 
         public float Generate(List<float> coordinates, int octaves = 1, float amplitude = 1f, float persistency = 0.6f, float frequencyGain = 0.5f, List<float> offsets = null, float initialFrequency = 1.0f)
         {
+            if (_baseNoises.Count < octaves)
+            {
+                for (int i = _baseNoises.Count; i < octaves; i++)
+                {
+                    _baseNoises.Add(new BaseSimplexNoise(_seed + i));
+                }
+            }
+
             if (offsets == null)
             {
                 offsets = new List<float>(coordinates.Count);
@@ -42,7 +53,7 @@ namespace antoinegleisberg.Math.ProceduralGeneration
             {
                 List<float> scaledCoords = offsetCoordinates.Select(x => x * initialFrequency).ToList();
 
-                totalNoise += currentPersistency * _baseNoise.CalculateNd(scaledCoords);
+                totalNoise += currentPersistency * _baseNoises[i].CalculateNd(scaledCoords);
 
                 initialFrequency *= frequencyGain;
                 currentPersistency *= persistency;
