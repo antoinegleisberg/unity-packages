72019a2d3bd368d3c2eca2e95e4af7bacb107354
Antoine Gleisberg <antoine.gleisberg@gmail.com>
2026-02-13 10:57:29 +0100

    optimising pathfinder with priority queue

--- Pathfinding/Runtime/Strategies/AStar.cs
+++ Pathfinding/Runtime/Strategies/AStar.cs
@@ -2,25 +2,25 @@ using antoinegleisberg.Types;
 using System;
 using System.Collections.Generic;
 
+using Utils;
+
 namespace antoinegleisberg.Pathfinding
 {
     internal class AStar<TNode> : IPathfindingStrategy<TNode> where TNode : class
     {
         private Dictionary<TNode, int> _gScore; // cost to reach that TNode
-        private Dictionary<TNode, int> _fScore; // g + h
         private Dictionary<TNode, int> _hScore; // heuristic distance to end
         private Dictionary<TNode, TNode> _parents;
         
         private Func<TNode, List<TNode>, int> _heuristicDistance;
-        private Func<TNode, List<Tuple<TNode, int>>> _neighboursDistances; // For each neighbour, the distance to it
+        private Func<TNode, IEnumerable<ValueTuple<TNode, int>>> _neighboursDistances; // For each neighbour, the distance to it
 
-        public AStar(Func<TNode, TNode, int> heuristicDistance, Func<TNode, List<Tuple<TNode, int>>> neighbours)
+        public AStar(Func<TNode, TNode, int> heuristicDistance, Func<TNode, IEnumerable<ValueTuple<TNode, int>>> neighbours)
         {
             _heuristicDistance = ConvertHeuristicDistance(heuristicDistance);
             _neighboursDistances = neighbours;
 
             _gScore = new Dictionary<TNode, int>();
-            _fScore = new Dictionary<TNode, int>();
             _hScore = new Dictionary<TNode, int>();
             _parents = new Dictionary<TNode, TNode>();
         }
@@ -33,59 +33,58 @@ namespace antoinegleisberg.Pathfinding
         public List<TNode> FindPath(List<TNode> startNodes, List<TNode> endNodes)
         {
             // ToDo: implement priority queue to optimise this
-            List<TNode> openList = new List<TNode>();
+            PriorityQueue<TNode, PathPriority> openSet = new PriorityQueue<TNode, PathPriority>();  // min-heap, smallest priority first
             HashSet<TNode> closedSet = new HashSet<TNode>();
 
             _gScore.Clear();
-            _fScore.Clear();
             _hScore.Clear();
             _parents.Clear();
 
             foreach (TNode startNode in startNodes)
             {
-                _gScore.Add(startNode, 0);
-                _hScore.Add(startNode, _heuristicDistance(startNode, endNodes));
-                _fScore.Add(startNode, _gScore[startNode] + _hScore[startNode]);
+                int g = 0;
+                int h = _heuristicDistance(startNode, endNodes);
+                int f = g + h;
+                _gScore.Add(startNode, g);
+                _hScore.Add(startNode, h);
                 _parents.Add(startNode, null);
 
-                openList.Add(startNode);
+                openSet.Enqueue(startNode, new PathPriority(f, h));
             }
 
-            while (openList.Count > 0)
+            while (openSet.Count > 0)
             {
-                TNode currentNode = GetLowestCostNode(openList);
+                TNode currentNode = openSet.Dequeue();
 
                 if (endNodes.Contains(currentNode))
                 {
                     return CalculatePath(currentNode);
                 }
 
-                openList.Remove(currentNode);
-                closedSet.Add(currentNode);
+                // If the node has already been processed, skip it
+                if (!closedSet.Add(currentNode)) continue;
 
-                foreach (Tuple<TNode, int> neighbourDistancePair in _neighboursDistances(currentNode))
+                foreach ((TNode neighbour, int distance) in  _neighboursDistances(currentNode))
                 {
-                    TNode neighbour = neighbourDistancePair.Item1;
-                    int distance = neighbourDistancePair.Item2;
-
                     if (closedSet.Contains(neighbour))
                     {
                         continue;
                     }
 
-                    if (!_parents.ContainsKey(neighbour))
+                    int tentativeGScore = _gScore[currentNode] + distance;
+
+                    // If we haven't seen this neighbor OR found a faster way to it
+                    if (!_gScore.ContainsKey(neighbour) || tentativeGScore < _gScore[neighbour])
                     {
                         _parents[neighbour] = currentNode;
-                        _gScore[neighbour] = int.MaxValue;
-                        _hScore[neighbour] = _heuristicDistance(neighbour, endNodes);
-                        _fScore[neighbour] = int.MaxValue;
-                    }
+                        _gScore[neighbour] = tentativeGScore;
+                        int h = _heuristicDistance(neighbour, endNodes);
+                        _hScore[neighbour] = h;
 
-                    UpdateCosts(currentNode, neighbour, distance);
+                        int fScore = tentativeGScore + h;  // fScore is f + g
 
-                    if (!openList.Contains(neighbour))
-                    {
-                        openList.Add(neighbour);
+                        // We may enqueue duplicates here. The while loop checks for duplicates in the closed set.
+                        openSet.Enqueue(neighbour, new PathPriority(fScore, h));
                     }
                 }
             }
@@ -93,52 +92,22 @@ namespace antoinegleisberg.Pathfinding
             // No path was found, the two nodes are not connected
             return null;
         }
-
-        private void UpdateCosts(TNode currentNode, TNode neighbour, int distance)
-        {
-            int potentialNewGCost = _gScore[currentNode] + distance;
-
-            if (potentialNewGCost < _gScore[neighbour])
-            {
-                _parents[neighbour] = currentNode;
-                _gScore[neighbour] = potentialNewGCost;
-                _fScore[neighbour] = _gScore[neighbour] + _hScore[neighbour];
-            }
-        }
         
 
         private List<TNode> CalculatePath(TNode end)
         {
             List<TNode> path = new List<TNode>();
             TNode currentNode = end;
-            while (_parents[currentNode] != null)
+            while (currentNode != null)
             {
-                path.Insert(0, currentNode);
-                currentNode = _parents[currentNode];
+                path.Add(currentNode);
+                if (!_parents.TryGetValue(currentNode, out currentNode))
+                {
+                    currentNode = null;
+                }
             }
-            path.Insert(0, currentNode);
+            path.Reverse();
             return path;
-        }
-
-        private TNode GetLowestCostNode(List<TNode> openList)
-        {
-            TNode lowestNode = openList[0];
-            foreach (TNode node in openList)
-            {
-                if (_fScore[node] < _fScore[lowestNode])
-                {
-                    lowestNode = node;
-                }
-
-                else if (_fScore[node] == _fScore[lowestNode])
-                {
-                    if (_hScore[node] < _hScore[lowestNode])
-                    {
-                        lowestNode = node;
-                    }
-                }
-            }
-            return lowestNode;
         }
 
         private static Func<TNode, List<TNode>, int> ConvertHeuristicDistance(Func<TNode, TNode, int> heuristicDistance)
@@ -158,6 +127,31 @@ namespace antoinegleisberg.Pathfinding
             };
 
             return heuristicFunction;
+        }
+    
+
+        private struct PathPriority : IComparable<PathPriority>
+        {
+            public int F;
+            public int H;
+
+            public PathPriority(int f, int h)
+            {
+                F = f;
+                H = h;
+            }
+
+            public int CompareTo(PathPriority other)
+            {
+                // First, compare F scores (Main priority)
+                int result = F.CompareTo(other.F);
+                if (result == 0)
+                {
+                    // If F is equal, compare H scores (Tie-breaker)
+                    result = H.CompareTo(other.H);
+                }
+                return result;
+            }
         }
     }
 }
